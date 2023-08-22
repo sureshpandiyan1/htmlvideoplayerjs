f5b0dd1521bb30e6862ebf38071f229292491030
Suresh P <112636345+sureshpandiyan1@users.noreply.github.com>
2023-08-22 16:18:38 +0530

    reedited

--- src/hvp.js
+++ src/hvp.js
@@ -24,7 +24,7 @@ function forwardstepN(startfrombegin, video, vbar) {
   const VIDEO = document.getElementById(video)
 
   sfb.addEventListener('click', function() {
-    VIDEO.currentTime = 0
+    VIDEO.currentTime = Math.floor(VIDEO.duration) / Math.floor(VIDEO.duration)
     document.getElementById(vbar).value = 0
   }, false)
 }
@@ -35,6 +35,14 @@ function volumeriserN(changevolume,video,cvolume,volumechange,vbar,folderpath) {
   const VIDEO = document.getElementById(video)
   VOLUMECHANGER.setAttribute('data-video',volumechange)
   let taponce = 0;
+
+
+  if (document.getElementById(video).ended != true) {
+    setInterval(function() {
+     document.getElementById(vbar).max = Math.floor(VIDEO.duration)
+     document.getElementById(vbar).value = Math.floor(VIDEO.currentTime)
+      }, 1000)
+  }
 
   const CVOLU = document.getElementById(cvolume)
   CVOLU.onclick = function() {
@@ -55,30 +63,29 @@ function volumeriserN(changevolume,video,cvolume,volumechange,vbar,folderpath) {
 
 }
 
-function videobarN(video,vbar) {
+
+
+function videobarN(video,vbar,videocontrols,targetid) {
   const VIDEOBARLENGTH = document.getElementById(vbar)
   const VIDEO = document.getElementById(video)
-
-
-
+  
   VIDEO.addEventListener('timeupdate', function() {
-    VIDEOBARLENGTH.max = Math.floor(VIDEO.duration)
+    const mnn = Math.floor(Math.floor(VIDEOBARLENGTH.value % 3600) / 60)
+    const timer = VIDEOBARLENGTH.value
+    var seconds = Math.floor(timer % 60) % 60
 
-    VIDEOBARLENGTH.value = Math.floor(VIDEO.currentTime)
+    var hrs = Math.floor(VIDEOBARLENGTH.value) / 60 / 60
+    var hh;
+    hh = Math.floor(VIDEOBARLENGTH.value < 3600) ? mnn+":"+seconds :  Math.floor(hrs)+":"+mnn+":"+seconds
+    document.getElementById(videocontrols+targetid+vbar).innerHTML = hh
   })
 
-  
-
-
   VIDEOBARLENGTH.addEventListener('input', function() {
-    VIDEO.pause()
     VIDEO.play()
     VIDEO.currentTime = VIDEOBARLENGTH.value
+  
   }, false)
-
-
 }
-
 
 
 function playspdzN(playbackspd,setz,video) {
@@ -87,9 +94,9 @@ function playspdzN(playbackspd,setz,video) {
       const SFIRST = document.getElementById(playbackspd)
       document.getElementById(setz).style.display = 'block'
       const INDEXER = SFIRST.selectedIndex
-      const OPTIONS = SFIRST.options
+      console.log(INDEXER)
 
-        const VIDEO = document.getElementById(video)
+      const VIDEO = document.getElementById(video)
 
       SFIRST.addEventListener('change', function(event) {
         VIDEO.playbackRate = event.target.value
@@ -100,6 +107,7 @@ function playspdzN(playbackspd,setz,video) {
 
 
 function allsettN(video,vsettings,setz,playbackspd,videoplayback,folderpath) {
+  
   const SETTINGS = document.getElementById(vsettings)
   const TCONTROLS = document.createElement('div')
   const CLOSE = document.createElement('div')
@@ -121,7 +129,6 @@ function allsettN(video,vsettings,setz,playbackspd,videoplayback,folderpath) {
   SFIRST.style.cssText = 'width: 57px; border: 1px solid gray; border-radius: 6px; padding: 3px; margin-top: -2px; background: #3c3c3c; color: white;/* border: none; */font-size: 15px;'
   SFIRST.setAttribute('id',playbackspd)
   var pyspeed = [0.4,0.6,0.8,1,1.2,1.4,1.6]
-
   for (let x of pyspeed) {
     const opt = document.createElement('option')
     opt.value = x
@@ -132,9 +139,6 @@ function allsettN(video,vsettings,setz,playbackspd,videoplayback,folderpath) {
   SETTINGS.appendChild(TCONTROLS)
   SPLAYBACK.appendChild(SFIRST)
   SPLAYBACK.appendChild(CLOSE)
-
-
-
   SETTINGS.addEventListener('mouseenter', function() {
     TCONTROLS.appendChild(SPLAYBACK)
     TCONTROLS.style.display = 'block'
@@ -148,8 +152,6 @@ function allsettN(video,vsettings,setz,playbackspd,videoplayback,folderpath) {
 }
 
 
-
-
 function htmlVideoplayer(folderpath,targetid,width,height,video,sourceurl,
                         videocontrols,volumecontrols,volumechange,vd1,playbackspd,playnpause,startfrombegin,videoplayback,
                         volume,vd2,videolength,vsettings,setz,vbar,pause,play,cvolume,changevolume) {
@@ -157,10 +159,9 @@ function htmlVideoplayer(folderpath,targetid,width,height,video,sourceurl,
         `<source src='${sourceurl}' type='video/mp4' codecs='avc1.42E01E, mp4a.40.2'>` +
         `<p>your browser doesnt support that</p>` +
         `</video>` +
-        `<div id='${videocontrols}'` +  `style='flex-direction: column; width: ${width.indexOf('%') > -1 ? width.split('%')[0] - 9+'%' : width - 20+'px'}; display: flex; gap: 10px; margin-top: -73px; padding: 9px; position: absolute;'>` +
-
-        `<div id='${vd1}'>` +
-        `<input type='range' min='0' value='0' class='lranger' id='${vbar}' style='-webkit-appearance: none; appearance: none; width: ${width.indexOf('%') > -1 ? width.split('%')[0] - 9+'%' : width - 20+'px'}; height: 9px; background: #fff; border-radius: 10px; outline: none; opacity: 0.7; -webkit-transition: .2s;  transition: opacity .2s;'>` +
+        `<div id='${videocontrols}'` +  `style='flex-direction: column; width: ${width.indexOf('%') > -1 ? width.split('%')[0] - 2+'%' : width - 20+'px'}; display: flex; gap: 10px; margin-top: -73px; padding: 9px; position: absolute;'>` +
+        `<div id='${vd1}'`+ `style='display: flex; flex-grow: 1;'>` +
+        `<input type='range' min='0' value='0' class='lranger' id='${vbar}' style='-webkit-appearance: none; appearance: none; width: 99%; height: 9px; background: #fff; border-radius: 10px; outline: none; opacity: 0.7; -webkit-transition: .2s;  transition: opacity .2s;'>` +
         `</div>` +
         `<div id='${vd2}' + ' style='display: flex; align-items: center; justify-content: flex-start; gap: 12px;'>` +
 
@@ -176,8 +177,9 @@ function htmlVideoplayer(folderpath,targetid,width,height,video,sourceurl,
         `<input type='range' min='5' max='100' class='vranger' id='${changevolume}' data-video='volumechange' style='-webkit-appearance: none;  appearance: none; width: 80px; height: 5px; background: #fff; border-radius: 10px; outline: none; opacity: 0.7; -webkit-transition: .2s; transition: opacity .2s;'>` +
         `</div>` +
         `</div>` +
+        `<div id='${videocontrols+targetid+vbar}' style='color: snow; font-family: sans-serif;'>0:0</div>` +
         `<div id='${videolength}'></div>` +
-        `<div id='${vsettings}' style='position: relative;'>` +
+        `<div id='${vsettings}' style='position: relative; display: flex; margin-left: auto; width: 0%; justify-content: end; padding-right: 10px;'>` +
         `<img src='${folderpath}/settings.svg' class='icon'>` +
         `<div id='${setz}' style='display: none; background: rgb(60, 60, 60); width: 100px; height: 100px; position: absolute; top: -130px; border-radius: 10px; padding: 10px;'></div>` +
         `</div>` +
@@ -186,15 +188,17 @@ function htmlVideoplayer(folderpath,targetid,width,height,video,sourceurl,
 
         playpauseN(video, playnpause, play, pause,folderpath)
         forwardstepN(startfrombegin, video, vbar)
-        videobarN(video,vbar)
+        videobarN(video,vbar,videocontrols,targetid)
         playspdzN(playbackspd,setz,video)
         volumeriserN(changevolume,video,cvolume,volumechange, vbar, folderpath)
         allsettN(video,vsettings,setz,playbackspd,videoplayback,folderpath)
+        
 }
 
 
 
 function ttid(tid) {
+  
 var rnds = ['ubgyuctuikvghu','zcgyugvucyzccx','mvgvuoubhkjjmz','xxkvligllfdsm','zcmwlkbvuh86rdf','asdf798y0bbrcasfd','asdhjjhvhcr9gfasdfewr','iukljonndsg67bbex7fufd',
 'sfiunjgviughbosadf','dsfinojluccgkhasdf','zcdfegvvpjnjnuhjggr12','i8u0gi97fohupgiu','if796fhknjbhkig8lfj','asd87fyuvv8yfasdfijkml',
 'asdy9hjictciyfas','jmkbjfyuvkb7gy9ibmi','adf8yg08y07iytasdfsadf','asdfsadf']
@@ -209,7 +213,7 @@ return z
 function htmlVideoPlayerjs(folderpath,tid,width,height,videoid,url) {
     htmlVideoplayer(folderpath,tid,width,height,videoid,url,...ttid(tid))
 }
-  
 
   
+
 export default htmlVideoPlayerjs;
